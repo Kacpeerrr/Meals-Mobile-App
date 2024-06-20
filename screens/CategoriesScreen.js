6c232bb8244f723c8a810ba2f0f4b7d135a776e4
Kacper <kacpeeer.mikolajczyk@gmail.com>
2024-06-20 19:23:38 +0200

    Add navigation and basic meals screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,19 +1,31 @@
 import { FlatList } from 'react-native'
-
-import { CATEGORIES } from '../data/dummy-data'
 import CategoryGridTile from '../components/CategoryGridTile'
 
-function renderCategoryItem(itemData) {
-	return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
-}
+import { CATEGORIES } from '../data/dummy-data'
 
-function CategoriesScreen() {
+function CategoriesScreen({ navigation }) {
+	function renderCategoryItem(itemData) {
+		function pressHandler() {
+            navigation.navigate('Podgląd posiłku', {
+                categoryId: itemData.item.id
+            })
+        }
+
+		return (
+			<CategoryGridTile
+				title={itemData.item.title}
+				color={itemData.item.color}
+				onPress={pressHandler}
+			/>
+		)
+	}
+
 	return (
 		<FlatList
 			data={CATEGORIES}
 			keyExtractor={item => item.id}
 			renderItem={renderCategoryItem}
-            numColumns={2}
+			numColumns={2}
 		/>
 	)
 }
